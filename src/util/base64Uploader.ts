cf586b3baf45934449758b235cf1f433da098b83
开源海哥 <fuhai999@gmail.com>
2023-11-11 09:51:02 +0800

    fix: base64Uploader accept result is error

--- src/util/base64Uploader.ts
+++ src/util/base64Uploader.ts
@@ -1,7 +1,7 @@
 export const base64Uploader = (file: File, _uploadUrl: string, _headers: Record<string, any>, _formName: string): Promise<Record<string, any>> => {
     let reader = new FileReader;
     return new Promise((accept, fail) => {
-        reader.onload = () => accept({src:reader.result as string});
+        reader.onload = () => accept({errorCode:0,data:{src:reader.result as string}});
         reader.onerror = () => fail(reader.error);
         setTimeout(() => reader.readAsDataURL(file), 1500);
     })
