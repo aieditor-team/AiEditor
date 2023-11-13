c3be09e7d9f6bb19aa8bfd4a174c2eaf1cf1b354
开源海哥 <fuhai999@gmail.com>
2023-11-13 20:26:44 +0800

    feat: add layui demos

---
+++ demos/layui/aieditor/index.d.ts
@@ -0,0 +1,149 @@
+import { ChainedCommands } from '@tiptap/core/dist/packages/core/src/types';
+import { Editor } from '@tiptap/core';
+import { EditorEvents } from '@tiptap/core';
+import { EditorOptions } from '@tiptap/core';
+import { JSONContent } from '@tiptap/core';
+
+declare class AbstractMenuButton extends HTMLElement implements AiEditorEvent {
+    template: string;
+    editor?: Editor;
+    options?: AiEditorOptions;
+    protected constructor();
+    protected registerClickListener(): void;
+    connectedCallback(): void;
+    onClick(commands: ChainedCommands): void;
+    onCreate(props: EditorEvents["create"], options: AiEditorOptions): void;
+    onTransaction(event: EditorEvents["transaction"]): void;
+    onActive(editor: Editor): boolean;
+}
+
+export declare interface AiCommand {
+    name: string;
+    keyword: string;
+    prompt: string;
+    model: string;
+}
+
+export declare class AiEditor {
+    innerEditor: InnerEditor;
+    container: HTMLDivElement;
+    menus: Header;
+    footer: Footer;
+    options: AiEditorOptions;
+    eventComponents: AiEditorEvent[];
+    constructor(_: AiEditorOptions);
+    onCreate(props: EditorEvents['create'], mainEl: Element): void;
+    onTransaction(props: EditorEvents['transaction']): void;
+    onDestroy(): void;
+    getHtml(): string;
+    getJson(): JSONContent;
+    getText(): string;
+}
+
+export declare interface AiEditorEvent {
+    onCreate: (props: EditorEvents['create'], options: AiEditorOptions) => void;
+    onTransaction: (props: EditorEvents['transaction']) => void;
+}
+
+export declare type AiEditorOptions = {
+    element: string | Element;
+    content?: string;
+    placeholder?: string;
+    theme?: "light" | "dark";
+    cbName?: string;
+    cbUrl?: string;
+    onMentionQuery?: (query: string) => any[] | Promise<any[]>;
+    onChange?: (editor: AiEditor) => void;
+    toolbarKeys?: string[];
+    uploader?: (file: File, uploadUrl: string, headers: Record<string, any>, formName: string) => Promise<Record<string, any>>;
+    image?: {
+        customMenuInvoke?: (editor: Editor) => void;
+        uploadUrl?: string;
+        uploadHeaders?: Record<string, any>;
+        uploader?: (file: File, uploadUrl: string, headers: Record<string, any>, formName: string) => Promise<Record<string, any>>;
+    };
+    video?: {
+        customMenuInvoke?: (editor: Editor) => void;
+        uploadUrl?: string;
+        uploadHeaders?: Record<string, any>;
+        uploader?: (file: File, uploadUrl: string, headers: Record<string, any>, formName: string) => Promise<Record<string, any>>;
+    };
+    attachment?: {
+        customMenuInvoke?: (editor: Editor) => void;
+        uploadUrl?: string;
+        uploadHeaders?: Record<string, any>;
+        uploader?: (file: File, uploadUrl: string, headers: Record<string, any>, formName: string) => Promise<Record<string, any>>;
+    };
+    fontFamily?: {
+        values: NameAndValue[];
+    };
+    fontSize?: {
+        values: NameAndValue[];
+    };
+    ai?: {
+        model: {
+            xinghuo?: {
+                protocol?: string;
+                appId: string;
+                apiKey: string;
+                apiSecret: string;
+                version?: string;
+                urlSignatureAlgorithm?: (model: XingHuoModel) => string;
+            };
+        };
+        menus?: AiMenu[];
+        command?: AiCommand[];
+    };
+};
+
+export declare interface AiMenu {
+    icon: string;
+    name: string;
+    prompt: string;
+    text: "selected" | "focusBefore";
+    model: string;
+}
+
+declare interface AiModel {
+    start: (seletedText: string, prompt: string, editor: Editor) => void;
+}
+
+declare class Footer extends HTMLElement implements AiEditorEvent {
+    count: number;
+    constructor();
+    updateCharacters(): void;
+    onCreate(props: EditorEvents["create"], _: AiEditorOptions): void;
+    onTransaction(props: EditorEvents["transaction"]): void;
+}
+
+declare class Header extends HTMLElement implements AiEditorEvent {
+    menuButtons: AbstractMenuButton[];
+    constructor();
+    connectedCallback(): void;
+    onCreate(event: EditorEvents["create"], options: AiEditorOptions): void;
+    onTransaction(event: EditorEvents["transaction"]): void;
+}
+
+export declare class InnerEditor extends Editor {
+    userOptions: AiEditorOptions;
+    constructor(userOptions: AiEditorOptions, options?: Partial<EditorOptions>);
+}
+
+export declare interface NameAndValue {
+    name: string;
+    value: any;
+}
+
+declare class XingHuoModel implements AiModel {
+    protocol: string;
+    appId: string;
+    apiKey: string;
+    apiSecret: string;
+    version: string;
+    urlSignatureAlgorithm: (model: XingHuoModel) => string;
+    constructor(options: AiEditorOptions);
+    start(seletedText: string, prompt: string, editor: Editor): void;
+    createUrl(): string;
+}
+
+export { }
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import {defineConfig} from 'vite'
 import {resolve} from 'path';
 import dts from 'vite-plugin-dts'
 
-
 export default defineConfig({
+    define: {'process.env': {}},
     build: {
         lib: {
             entry: resolve(__dirname, './src/index.ts'),
@@ -12,18 +12,6 @@ export default defineConfig({
             fileName: `index`,
             formats: ['es', 'cjs']
         },
-        rollupOptions: {
-            // 排除不相关的依赖
-            // external: [],
-            // output: {
-            //     // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-            //     globals: {
-            //         react: 'React',
-            //     },
-            // },
-        },
-        // outDir: 'dist',
-
     },
     plugins: [dts({rollupTypes: true})],
 })
