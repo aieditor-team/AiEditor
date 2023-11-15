55188bafe0ab61c45abff1e5ad229d0a9bd70425
开源海哥 <fuhai999@gmail.com>
2023-11-15 11:14:03 +0800

    feat: add text tooltips for bubble menu

--- src/components/AbstractBubbleMenu.ts
+++ src/components/AbstractBubbleMenu.ts
@@ -1,12 +1,14 @@
 import {AiEditorOptions, AiEditorEvent} from "../core/AiEditor.ts";
 import {Editor, EditorEvents} from "@tiptap/core";
+import tippy from "tippy.js";
 
 export type BubbleMenuItem = {
     id: string,
+    title?: string,
     svg: string,
 }
 
-export abstract class AbstractBubbleMenu extends HTMLElement implements AiEditorEvent{
+export abstract class AbstractBubbleMenu extends HTMLElement implements AiEditorEvent {
 
     editor?: Editor;
     items: BubbleMenuItem[] = [];
@@ -20,10 +22,8 @@ export abstract class AbstractBubbleMenu extends HTMLElement implements AiEditor
         this.innerHTML = `
             <div class="aie-bubble-menu">
                ${this.items!.map((item) => {
-                    return `
-                   <div class="aie-bubble-menu-item" id="${item.id}">${item.svg}</div>
-                   `
-                }).join('')}
+            return `<div class="aie-bubble-menu-item" id="${item.id}">${item.svg}</div>`
+        }).join('')}
             </div>
         `;
 
@@ -33,15 +33,29 @@ export abstract class AbstractBubbleMenu extends HTMLElement implements AiEditor
                 if (target) this.onItemClick(item.id);
             })
         })
+
+        this.querySelectorAll(".aie-bubble-menu-item").forEach((el, index) => {
+            const title = this.items[index].title;
+            if (title) {
+                tippy(el, {
+                    appendTo: this.closest(".aie-container")!,
+                    content: title,
+                    theme: 'aietip',
+                    arrow: true,
+                    // trigger:"click",
+                    // interactive:true,
+                });
+            }
+        });
     }
 
-    onCreate(props: EditorEvents['create'], _: AiEditorOptions){
+    onCreate(props: EditorEvents['create'], _: AiEditorOptions) {
         this.editor = props.editor
     }
 
     abstract onItemClick(id: string): void;
 
-    abstract onTransaction(props: EditorEvents['transaction']):void
+    abstract onTransaction(props: EditorEvents['transaction']): void
 
 }
 
