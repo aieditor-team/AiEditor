abde6e9f7e6705722187a827485ed188ca4884ef
开源海哥 <fuhai999@gmail.com>
2023-12-11 13:07:25 +0800

    refactor: update demos

--- demos/docs-ts/src/main.ts
+++ demos/docs-ts/src/main.ts
@@ -5,24 +5,28 @@ const content = `
 {"type":"doc","content":[{"type":"heading","attrs":{"level":2},"content":[{"type":"text","text":"AiEditor 是一个面向 AI 的下一代富文本编辑器。"}]},{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0},"content":[{"type":"text","marks":[{"type":"bold"}],"text":"提示："},{"type":"text","text":" "}]},{"type":"orderedList","attrs":{"tight":true,"start":1},"content":[{"type":"listItem","attrs":{"indent":0},"content":[{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0},"content":[{"type":"text","text":"输入 空格 + \\"/\\" 可以快速弹出 AI 菜单 "}]}]},{"type":"listItem","attrs":{"indent":0},"content":[{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0},"content":[{"type":"text","text":"输入 空格 + \\"@\\" 可以提及某人"}]}]}]},{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0}},{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0},"content":[{"type":"text","text":"请使用 Java 帮我写一个 hello world，只需要返回 java 代码内容"}]},{"type":"codeBlock","attrs":{"language":"java"},"content":[{"type":"text","text":"public class HelloWorld {\\n    public static void main(String[] args) {\\n        System.out.println(\\"Hello, World!\\");\\n    }\\n}"}]},{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0}}]}
 `
 
-// function updateOutLine(editor:AiEditor){
-//     const outlineContainer = document.querySelector("#outline");
-//     for (let child of outlineContainer!.children) {
-//         child.remove();
-//     }
-//     const outlines = editor.getOutline();
-//     for (let outline of outlines) {
-//         const child = document.createElement("div")
-//         child.classList.add(`aie-title${outline.level}`)
-//         child.style.marginLeft=`${14 * (outline.level - 1)}px`
-//         child.innerHTML = `<a href="#">${outline.text}</a>`
-//         child.addEventListener("click",(e)=>{
-//             e.preventDefault()
-//             editor.focusPos(outline.pos)
-//         })
-//         outlineContainer?.appendChild(child)
-//     }
-// }
+function updateOutLine(editor:AiEditor){
+    const outlineContainer = document.querySelector("#outline");
+    while (outlineContainer?.firstChild){
+        outlineContainer.firstChild.remove()
+    }
+    const outlines = editor.getOutline();
+    for (let outline of outlines) {
+        const child = document.createElement("div")
+        child.classList.add(`aie-title${outline.level}`)
+        child.style.marginLeft = `${14 * (outline.level - 1)}px`
+        child.innerHTML = `<a href="#${outline.id}">${outline.text}</a>`
+        child.addEventListener("click", (e) => {
+            e.preventDefault();
+            const el = editor.innerEditor.view.dom.querySelector(`#${outline.id}`) as HTMLElement;
+            el.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+            setTimeout(()=>{
+                editor.focusPos(outline.pos + outline.size - 1)
+            },1000)
+        })
+        outlineContainer?.appendChild(child)
+    }
+}
 
 // @ts-ignore
 window.aiEditor = new AiEditor({
@@ -46,10 +50,11 @@ window.aiEditor = new AiEditor({
         ].filter(item => item.toLowerCase().startsWith(query.toLowerCase())).slice(0, 5)
     },
 
-    // onCreated:(editor)=>{
-    // },
-    // onChange:(editor)=>{
-    //     // console.log(editor.getOutline())
-    //     // updateOutLine(editor)
-    // },
+    onCreated:(editor)=>{
+        updateOutLine(editor)
+    },
+    onChange:(editor)=>{
+        // console.log(editor.getOutline())
+        updateOutLine(editor)
+    },
 })
