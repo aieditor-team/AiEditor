fb2de9eb038650bd6b65f7dfb0bfe1383fbb8a02
简工作室 <837594412@qq.com>
2023-12-26 16:58:25 +0800

    update ai bubble menus

--- src/components/bubbles/TextSelectionBubbleMenu.ts
+++ src/components/bubbles/TextSelectionBubbleMenu.ts
@@ -138,10 +138,21 @@ export class TextSelectionBubbleMenu extends AbstractBubbleMenu {
         const container = document.createElement("div");
         container.classList.add("aie-ai-panel")
         container.innerHTML = `
-        <div class="aie-ai-panel-content"><div class="loader">${Svgs.refresh}</div><textarea readonly></textarea></div>
-        <div class="aie-ai-panel-actions"><button type="button" id="insert">追加</button><button type="button" id="replace">替换</button></div>
-        <div class="aie-ai-panel-input"><input id="prompt" placeholder="告诉 AI 下一步应该如何？比如：帮我翻译成英语" type="text" />
-        <button type="button" id="go" style="width: 30px;height: 30px">${Svgs.aiPanelStart}</button></div>
+        <div class="aie-ai-panel-body">
+            <div class="aie-ai-panel-body-content"><div class="loader">${Svgs.refresh}</div><textarea readonly></textarea></div>
+            <div class="aie-ai-panel-body-actions"><button type="button" id="insert">追加</button><button type="button" id="replace">替换</button></div>
+            <div class="aie-ai-panel-body-input"><input id="prompt" placeholder="告诉 AI 下一步应该如何？比如：帮我翻译成英语" type="text" />
+            <button type="button" id="go" style="width: 30px;height: 30px">${Svgs.aiPanelStart}</button></div>
+            <div class="aie-ai-panel-body-tips"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9 17C9 17 16 18 19 21H20C20.5523 21 21 20.5523 21 20V13.937C21.8626 13.715 22.5 12.9319 22.5 12C22.5 11.0681 21.8626 10.285 21 10.063V4C21 3.44772 20.5523 3 20 3H19C16 6 9 7 9 7H5C3.89543 7 3 7.89543 3 9V15C3 16.1046 3.89543 17 5 17H6L7 22H9V17ZM11 8.6612C11.6833 8.5146 12.5275 8.31193 13.4393 8.04373C15.1175 7.55014 17.25 6.77262 19 5.57458V18.4254C17.25 17.2274 15.1175 16.4499 13.4393 15.9563C12.5275 15.6881 11.6833 15.4854 11 15.3388V8.6612ZM5 9H9V15H5V9Z" fill="currentColor"></path></svg>
+            提示：您可以在上面输入文字或者选择下方的操作</div>
+        </div>
+        <div class="aie-ai-panel-footer">
+        <div class="aie-ai-panel-footer-tips">您可以进行以下操作:</div>
+        <p><a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2 18H12V20H2V18ZM2 11H22V13H2V11ZM2 4H22V6H2V4ZM18 18V15H20V18H23V20H20V23H18V20H15V18H18Z" fill="currentColor"></path></svg> 追加</a></p>
+        <p><a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10.071 4.92902L11.4852 6.34323L6.82834 11.0001L16.0002 11.0002L16.0002 13.0002L6.82839 13.0001L11.4852 17.6569L10.071 19.0712L2.99994 12.0001L10.071 4.92902ZM18.0001 19V5.00003H20.0001V19H18.0001Z" fill="currentColor"></path></svg> 替换</a></p>
+        <hr/>
+        <p><a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 4V6H15V4H9Z" fill="currentColor"></path></svg> 放弃</a></p>
+        </div>
         `;
 
         container.querySelector("#replace")!.addEventListener("click", () => {
