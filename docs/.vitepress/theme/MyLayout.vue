e78123fefa894ac31e2e3cff896f49f34bd6771e
michael <fuhai999@gmail.com>
2024-01-09 10:24:36 +0800

    doc: update docs

---
+++ docs/.vitepress/theme/MyLayout.vue
@@ -0,0 +1,38 @@
+<!--
+  -  Copyright (c) 2022-2023, Mybatis-Flex (fuhai999@gmail.com).
+  -  <p>
+  -  Licensed under the Apache License, Version 2.0 (the "License");
+  -  you may not use this file except in compliance with the License.
+  -  You may obtain a copy of the License at
+  -  <p>
+  -  http://www.apache.org/licenses/LICENSE-2.0
+  -  <p>
+  -  Unless required by applicable law or agreed to in writing, software
+  -  distributed under the License is distributed on an "AS IS" BASIS,
+  -  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  -  See the License for the specific language governing permissions and
+  -  limitations under the License.
+  -->
+
+<!--.vitepress/theme/MyLayout.vue-->
+
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+const {Layout} = DefaultTheme
+</script>
+
+
+<template>
+    <Layout>
+
+        <!--docs: https://vitepress.dev/guide/extending-default-theme#layout-slots-->
+        <template #doc-before>
+            <div style="margin-bottom: 30px">
+                <a href="https://mp.weixin.qq.com/s/V34VkARgBCBKdytSlKhosg" target="_blank">
+                    <img src="/assets/image/admin-banner.jpg">
+                </a>
+            </div>
+        </template>
+
+    </Layout>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,14 +2,11 @@
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import './style.css'
+import MyLayout from "./MyLayout.vue";
 
 export default {
   ...Theme,
-  Layout: () => {
-    return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  Layout: MyLayout,
   enhanceApp({ app, router, siteData }) {
     // ...
   }
