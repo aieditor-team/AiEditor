3dd71b7da0ad5bd8d585a423913eb5efd311349f
Michael Yang <fuhai999@gmail.com>
2024-03-17 15:43:21 +0800

    test: test for custom AI model

--- demos/vanilla-ts/src/main.ts
+++ demos/vanilla-ts/src/main.ts
@@ -1,5 +1,6 @@
 import {AiEditor} from "../../../src";
-import {config} from "./xinghuo.ts"
+// import {config} from "./xinghuo.ts"
+// import {AiMessage} from "../../../src/ai/core/AiMessage.ts";
 
 const content = `
 {"type":"doc","content":[{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0},"content":[{"type":"text","text":"AiEditor 是一个面向 AI 的下一代富文本编辑器。"}]},{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0},"content":[{"type":"text","marks":[{"type":"bold"}],"text":"提示："},{"type":"text","text":" "}]},{"type":"orderedList","attrs":{"tight":true,"start":1},"content":[{"type":"listItem","attrs":{"indent":0},"content":[{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0},"content":[{"type":"text","text":"输入 空格 + \\"/\\" 可以快速弹出 AI 菜单 "}]}]},{"type":"listItem","attrs":{"indent":0},"content":[{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0},"content":[{"type":"text","text":"输入 空格 + \\"@\\" 可以提及某人"}]}]}]},{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0}},{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0},"content":[{"type":"text","text":"请使用 Java 帮我写一个 hello world，只需要返回 java 代码内容"}]},{"type":"codeBlock","attrs":{"language":"java"},"content":[{"type":"text","text":"public class HelloWorld {\\n    public static void main(String[] args) {\\n        System.out.println(\\"Hello, World!\\");\\n    }\\n}"}]},{"type":"paragraph","attrs":{"lineHeight":"100%","textAlign":"left","indent":0}}]}
@@ -12,10 +13,26 @@ window.aiEditor = new AiEditor({
     content: JSON.parse(content),
     ai: {
         models: {
-            spark: {
-                ...config
+            // spark: {
+            //     ...config
+            // }
+            custom: {
+                url: "http://127.0.0.1:8080/api/v1/ai/chat",
+                messageWrapper: (message: string) => {
+                    return JSON.stringify({prompt: message})
+                },
+                messageParser: (message: string) => {
+                    return {
+                        role: "assistant",
+                        content: message,
+                        // index: number,
+                        // //0 代表首个文本结果；1 代表中间文本结果；2 代表最后一个文本结果。
+                        // status: 0|1|2,
+                    }
+                },
+                // protocol: "sse" | "websocket"
             }
-        }
+        },
     },
     onMentionQuery: (query: string) => {
         return [
