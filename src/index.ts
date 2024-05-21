463145881460d152a54d99a5314eea42ee3bcf1a
Michael Yang <fuhai999@gmail.com>
2024-05-21 16:55:05 +0800

    feat: export more classes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+
 export * from './core/AiEditor.ts';
 export * from './ai/AiModelManager.ts';
 export * from './ai/spark/SparkAiModel.ts';
+export * from './commons/defineCustomElement.ts';
+export * from './components/AbstractBubbleMenu.ts';
+export * from './components/AbstractColorsMenuButton.ts';
+export * from './components/AbstractDropdownMenuButton.ts';
+export * from './components/AbstractMenuButton.ts';
+export * from './components/bubbles/TextSelectionBubbleMenu.ts';
+export * from './components/bubbles/ImageBubbleMenu.ts';
+export * from './components/bubbles/LinkBubbleMenu.ts';
+export * from './components/bubbles/TableBubbleMenu.ts';
