982312e78306604b638e47eeb542cb1358d05e5c
Michael Yang <fuhai999@gmail.com>
2024-09-01 11:16:15 +0800

    chore: code format

--- src/util/base64Uploader.ts
+++ src/util/base64Uploader.ts
@@ -1,7 +1,7 @@
 export const base64Uploader = (file: File, _uploadUrl: string, _headers: Record<string, any>, _formName: string): Promise<Record<string, any>> => {
     let reader = new FileReader;
     return new Promise((accept, fail) => {
-        reader.onload = () => accept({errorCode:0,data:{src:reader.result,href:file.name}});
+        reader.onload = () => accept({errorCode: 0, data: {src: reader.result, href: file.name}});
         reader.onerror = () => fail(reader.error);
         setTimeout(() => reader.readAsDataURL(file), 5000 * Math.random());
     })
