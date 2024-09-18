f05a5f3a163a0f2afe9d7761e4e47ac2371a16ed
Tobias Krais <tk@design-to-use.de>
2024-09-18 09:39:49 +0200

    Create de.ts
    add german lang file

---
+++ src/i18n/de.ts
@@ -0,0 +1,122 @@
+export const de =  {
+    //// commons
+    "default": "Standard",
+    "delete": "Löschen",
+    "confirm": "Bestätigen",
+    "cancel": "Abbrechen",
+    "resize": "Größe ändern",
+
+    // menu tips
+    "undo": "Rückgängig",
+    "redo": "Wiederholen",
+    "brush": "Format übertragen",
+    "container": "Auswahl hervorheben",
+    "eraser": "Format löschen",
+    "heading": "Absatz/Überschrift",
+    "font-family": "Schriftart",
+    "font-size": "Schriftgröße",
+    "bold": "Fett",
+    "italic": "Kursiv",
+    "underline": "Unterstrichen",
+    "strike": "Durchgestrichen",
+    "link": "Link",
+    "source-code": "Quellcode",
+    "code": "Inline-Code",
+    "subscript": "Tiefgestellt",
+    "superscript": "Hochgestellt",
+    "hr": "Trennlinie",
+    "todo": "Aufgabenliste",
+    "emoji": "Emoji-Ausdruck",
+    "highlight": "Hervorheben",
+    "font-color": "Schriftfarbe",
+    "align": "Ausrichtung",
+    "line-height": "Zeilenhöhe",
+    "bullet-list": "Aufzählungsliste",
+    "ordered-list": "Nummerierte Liste",
+    "indent-decrease": "Einzug verkleinern",
+    "indent-increase": "Einzug vergrößern",
+    "break": "Zeilenumbruch erzwingen",
+    "image": "Bild",
+    "video": "Video",
+    "attachment": "Anhang",
+    "quote": "Zitat",
+    "code-block": "Codeblock",
+    "table": "Tabelle",
+    "printer": "Drucken",
+    "fullscreen": "Vollbild",
+    "ai": "KI",
+    "menu-group": "Menügruppe",
+
+    ///// paragraph selection
+    "paragraph": "Absatz",
+    "h1": "Überschrift 1",
+    "h2": "Überschrift 2",
+    "h3": "Überschrift 3",
+    "h4": "Überschrift 4",
+    "h5": "Überschrift 5",
+    "h6": "Überschrift 6",
+
+    //// colors panel
+    "standardColors": "Standardfarben",
+    "historyColors": "Zuletzt verwendete Farben",
+    
+    //// table
+    "insertTable": "Tabelle einfügen",
+    "row": "Zeile",
+    "column": "Spalte",
+    
+    //// alignment
+    "align-left": "Linksbündig",
+    "align-right": "Rechtsbündig",
+    "align-center": "Zentriert",
+    "align-justify": "Blocksatz",
+    
+    //// table
+    "insert-column-left": "Spalte links einfügen",
+    "insert-column-right": "Spalte rechts einfügen",
+    "insert-row-top": "Zeile oben einfügen",
+    "insert-row-bottom": "Zeile unten einfügen",
+    "delete-column": "Spalte löschen",
+    "delete-row": "Zeile löschen",
+    "merge-cells-horizontal": "Zellen horizontal verbinden",
+    "merge-cells-vertical": "Zellen vertikal verbinden",
+    "split-cells-horizontal": "Zellen horizontal teilen",
+    "split-cells-vertical": "Zellen vertikal teilen",
+    "delete-table": "Tabelle löschen",
+
+    /// link
+    "edit-link": "Link bearbeiten",
+    "unlink": "Link entfernen",
+    "visit-link": "Link besuchen",
+    "link-address": "Link-Adresse",
+    "link-open-type": "Link öffnen in...",
+    "link-open-blank": "Neues Fenster",
+    
+    //// font
+    "default-font-size": "Schriftgröße",
+    "default-font-family": "Standardschriftart",
+    
+    //// code block
+    "codeblock-ai-auto-comment": "Automatischer Kommentar",
+    "codeblock-ai-code-explain": "Code-Erklärung",
+    
+    //// ai
+    "placeholder-tell-ai-what-to-do-next": "Sagen Sie der KI, was als Nächstes zu tun ist, z.B. helfen Sie mir, ins Englische zu übersetzen",
+    "tip-you-can-enter-text-above-or-select-the-operation-below": "Tipp: Sie können oben Text eingeben oder die untenstehende Operation auswählen",
+    "you-can-do-the-following": "Sie können Folgendes tun:",
+    "improve-writing": "Schreiben verbessern",
+    "check-spelling-and-grammar": "Rechtschreibung und Grammatik prüfen",
+    "make-shorter": "Kürzen",
+    "make-longer": "Verlängern",
+    "translate": "Übersetzen",
+    "summarize": "Zusammenfassen",
+    "ai-append": "Anhängen",
+    "ai-replace": "Ersetzen",
+    "ai-cancel": "Abbrechen",
+    
+    /// ai menus
+    "ai-continuation": "Vorschläge für Fortsetzung",
+    "ai-optimization": "Optimierung",
+    "ai-proofreading": "Korrekturlesen",
+    "ai-translation": "Übersetzung",
+}
