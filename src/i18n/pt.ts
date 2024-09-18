37d3b3ad403bf80da23c6de58ce1eeb8c12370c9
Tobias Krais <tk@design-to-use.de>
2024-09-18 09:40:26 +0200

    Create pt.ts
    add portugese lang file

---
+++ src/i18n/pt.ts
@@ -0,0 +1,122 @@
+export const pt =  {
+    //// commons
+    "default": "Padrão",
+    "delete": "Excluir",
+    "confirm": "Confirmar",
+    "cancel": "Cancelar",
+    "resize": "Redimensionar",
+    
+    // menu tips
+    "undo": "Desfazer",
+    "redo": "Refazer",
+    "brush": "Pincel",
+    "container": "Destacar seleção",
+    "eraser": "Apagar formatação",
+    "heading": "Cabeçalho",
+    "font-family": "Fonte",
+    "font-size": "Tamanho da fonte",
+    "bold": "Negrito",
+    "italic": "Itálico",
+    "underline": "Sublinhado",
+    "strike": "Tachado",
+    "link": "Link",
+    "source-code": "Código-fonte",
+    "code": "Código em linha",
+    "subscript": "Subscrito",
+    "superscript": "Sobrescrito",
+    "hr": "Linha horizontal",
+    "todo": "Lista de tarefas",
+    "emoji": "Emoji",
+    "highlight": "Destacar",
+    "font-color": "Cor da fonte",
+    "align": "Alinhamento",
+    "line-height": "Altura da linha",
+    "bullet-list": "Lista com marcadores",
+    "ordered-list": "Lista numerada",
+    "indent-decrease": "Diminuir recuo",
+    "indent-increase": "Aumentar recuo",
+    "break": "Quebra de linha",
+    "image": "Imagem",
+    "video": "Vídeo",
+    "attachment": "Anexo",
+    "quote": "Citação",
+    "code-block": "Bloco de código",
+    "table": "Tabela",
+    "printer": "Imprimir",
+    "fullscreen": "Tela cheia",
+    "ai": "IA",
+    "menu-group": "Grupo de menu",
+    
+    ///// paragraph selection
+    "paragraph": "Parágrafo",
+    "h1": "Cabeçalho 1",
+    "h2": "Cabeçalho 2",
+    "h3": "Cabeçalho 3",
+    "h4": "Cabeçalho 4",
+    "h5": "Cabeçalho 5",
+    "h6": "Cabeçalho 6",
+    
+    //// colors panel
+    "standardColors": "Cores padrão",
+    "historyColors": "Cores recentes",
+    
+    //// table
+    "insertTable": "Inserir tabela",
+    "row": "Linha",
+    "column": "Coluna",
+    
+    //// alignment
+    "align-left": "Alinhar à esquerda",
+    "align-right": "Alinhar à direita",
+    "align-center": "Centralizar",
+    "align-justify": "Justificar",
+    
+    //// table
+    "insert-column-left": "Inserir coluna à esquerda",
+    "insert-column-right": "Inserir coluna à direita",
+    "insert-row-top": "Inserir linha acima",
+    "insert-row-bottom": "Inserir linha abaixo",
+    "delete-column": "Excluir coluna",
+    "delete-row": "Excluir linha",
+    "merge-cells-horizontal": "Mesclar células horizontalmente",
+    "merge-cells-vertical": "Mesclar células verticalmente",
+    "split-cells-horizontal": "Dividir células horizontalmente",
+    "split-cells-vertical": "Dividir células verticalmente",
+    "delete-table": "Excluir tabela",
+    
+    /// link
+    "edit-link": "Editar link",
+    "unlink": "Remover link",
+    "visit-link": "Visitar link",
+    "link-address": "Endereço do link",
+    "link-open-type": "Abrir link em...",
+    "link-open-blank": "Nova janela",
+    
+    //// font
+    "default-font-size": "Tamanho padrão da fonte",
+    "default-font-family": "Fonte padrão",
+    
+    //// code block
+    "codeblock-ai-auto-comment": "Comentário automático",
+    "codeblock-ai-code-explain": "Explicação do código",
+    
+    //// ai
+    "placeholder-tell-ai-what-to-do-next": "Diga à IA o que fazer a seguir, por exemplo, ajude-me a traduzir para o inglês",
+    "tip-you-can-enter-text-above-or-select-the-operation-below": "Dica: Você pode digitar o texto acima ou selecionar a operação abaixo",
+    "you-can-do-the-following": "Você pode fazer o seguinte:",
+    "improve-writing": "Melhorar escrita",
+    "check-spelling-and-grammar": "Verificar ortografia e gramática",
+    "make-shorter": "Encurtar",
+    "make-longer": "Alongar",
+    "translate": "Traduzir",
+    "summarize": "Resumir",
+    "ai-append": "Anexar",
+    "ai-replace": "Substituir",
+    "ai-cancel": "Cancelar",
+    
+    /// ai menus
+    "ai-continuation": "Sugestões de continuação",
+    "ai-optimization": "Otimização",
+    "ai-proofreading": "Revisão",
+    "ai-translation": "Tradução",
+}
