ee0c288295096704cb4d1bc2ec94df6cb72c626f
Michael Yang <fuhai999@gmail.com>
2025-03-20 10:05:09 +0800

    feat: add support table cell bgColor and color attrs

--- src/extensions/TableHeaderExt.ts
+++ src/extensions/TableHeaderExt.ts
@@ -1,21 +1,47 @@
-import { TableHeader } from '@tiptap/extension-table-header';
+import {TableHeader} from '@tiptap/extension-table-header';
 
 export const TableHeaderExt = TableHeader.extend({
-  addAttributes() {
-    return {
-      ...this.parent?.(), // 继承默认属性
-      backgroundColor: {
-        default: null, // 默认没有背景色
-        parseHTML: (element) => element.style.backgroundColor || null,
-        renderHTML: (attributes) => {
-          if (!attributes.backgroundColor) {
-            return {};
-          }
-          return {
-            style: `background-color: ${attributes.backgroundColor}`,
-          };
-        },
-      },
-    };
-  },
+    addAttributes() {
+        return {
+            ...this.parent?.(),
+            backgroundColor: {
+                default: null,
+                parseHTML: (element) => element.style.backgroundColor || null,
+                renderHTML: (attributes) => {
+                    if (!attributes.backgroundColor) {
+                        return {};
+                    }
+                    return {
+                        style: `background-color: ${attributes.backgroundColor}`,
+                    };
+                },
+            },
+
+            color: {
+                default: null,
+                parseHTML: (element) => element.style.color || null,
+                renderHTML: (attributes) => {
+                    if (!attributes.color) {
+                        return {};
+                    }
+                    return {
+                        style: `color: ${attributes.color}`,
+                    };
+                },
+            },
+
+            width: {
+                default: null,
+                parseHTML: (element) => element.style.width || null,
+                renderHTML: (attributes) => {
+                    if (!attributes.width) {
+                        return {};
+                    }
+                    return {
+                        style: `width: ${attributes.width}`,
+                    };
+                },
+            },
+        };
+    },
 });
